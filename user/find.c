b97fd5b38d3d0a43f46da1dadb71a1e70e05067c
Sudh <sudheerrathore04@gmail.com>
2026-09-14 23:29:24 +0500

    adding regex support to find

--- user/find.c
+++ user/find.c
@@ -5,10 +5,59 @@
 #include "kernel/fcntl.h"
 #include "kernel/param.h"
 
+// Regular expression matcher.
+// Supports: ^ . * $
+int matchhere(char *, char *);
+int matchstar(int, char *, char *);
+
+int
+match(char *re, char *text)
+{
+  if (re[0] == '^')
+    return matchhere(re + 1, text);
+
+  do {
+    if (matchhere(re, text))
+      return 1;
+  } while (*text++ != '\0');
+
+  return 0;
+}
+
+int
+matchhere(char *re, char *text)
+{
+  if (re[0] == '\0')
+    return 1;
+
+  if (re[1] == '*')
+    return matchstar(re[0], re + 2, text);
+
+  if (re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+
+  if (*text != '\0' && (re[0] == '.' || re[0] == *text))
+    return matchhere(re + 1, text + 1);
+
+  return 0;
+}
+
+int
+matchstar(int c, char *re, char *text)
+{
+  do {
+    if (matchhere(re, text))
+      return 1;
+  } while (*text != '\0' && (*text++ == c || c == '.'));
+
+  return 0;
+}
+
 void
 find(char *path, char *target, char *cmd[], int ncmd)
 {
   char buf[512], *p;
+  char *name;
   int fd;
   struct dirent de;
   struct stat st;
@@ -24,41 +73,41 @@ find(char *path, char *target, char *cmd[], int ncmd)
     return;
   }
 
-  if (strcmp(path + strlen(path) - strlen(target), target) == 0) {
-    char *name = path + strlen(path);
-    while (name > path && name[-1] != '/')
-      name--;
+  // Get just the filename from the path.
+  name = path + strlen(path);
+  while (name > path && name[-1] != '/')
+    name--;
 
-    if (strcmp(name, target) == 0) {
-      if (ncmd == 0) {
-        printf("%s\n", path);
+  // Check the filename against the regular expression.
+  if (match(target, name)) {
+    if (ncmd == 0) {
+      printf("%s\n", path);
+    } else {
+      char *args[MAXARG];
+      int i;
+
+      if (ncmd + 1 >= MAXARG) {
+        fprintf(2, "find: too many arguments\n");
+        close(fd);
+        return;
+      }
+
+      for (i = 0; i < ncmd; i++)
+        args[i] = cmd[i];
+
+      args[ncmd] = path;
+      args[ncmd + 1] = 0;
+
+      int pid = fork();
+
+      if (pid < 0) {
+        fprintf(2, "find: fork failed\n");
+      } else if (pid == 0) {
+        exec(args[0], args);
+        fprintf(2, "find: exec %s failed\n", args[0]);
+        exit(1);
       } else {
-        char *args[MAXARG];
-        int i;
-
-        if (ncmd + 1 >= MAXARG) {
-          fprintf(2, "find: too many arguments\n");
-          close(fd);
-          return;
-        }
-
-        for (i = 0; i < ncmd; i++)
-          args[i] = cmd[i];
-
-        args[ncmd] = path;
-        args[ncmd + 1] = 0;
-
-        int pid = fork();
-
-        if (pid < 0) {
-          fprintf(2, "find: fork failed\n");
-        } else if (pid == 0) {
-          exec(args[0], args);
-          fprintf(2, "find: exec %s failed\n", args[0]);
-          exit(1);
-        } else {
-          wait(0);
-        }
+        wait(0);
       }
     }
   }
